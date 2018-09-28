c978343a5050855908b12cad35b3c6423f963db8
Mith <25213249+MrMith@users.noreply.github.com>
2018-09-28 15:46:16 -0500

    Added SCP343_nuke_interact config option and SpawnSCP343 server command.

--- SCP-343/SCP-343.cs
+++ SCP-343/SCP-343.cs
@@ -5,6 +5,7 @@ using Smod2.EventHandlers;
 using Smod2.Events;
 using Smod2.EventSystem.Events;
 using Smod2.API;
+using Smod2.Commands;
 using System;
 using System.Collections.Generic;
 
@@ -44,19 +45,22 @@ namespace SCP_343
 			this.AddEventHandler(typeof(IEventHandlerRoundStart), new MainLogic(this), Priority.Normal);
 			this.AddEventHandler(typeof(IEventHandlerDoorAccess), new MainLogic(this), Priority.Normal);
 			this.AddEventHandler(typeof(IEventHandlerSetRole), new MainLogic(this), Priority.Normal);
+			this.AddEventHandler(typeof(IEventHandlerWarheadStartCountdown), new MainLogic(this), Priority.Normal);
+			this.AddEventHandler(typeof(IEventHandlerWarheadStopCountdown), new MainLogic(this), Priority.Normal);
+            this.AddCommand("SpawnSCP343", new SCP_343Commands.SpawnSCP343(this));
 
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_spawnchance", 10f, Smod2.Config.SettingType.FLOAT, true, "Percent chance for SPC-343 to spawn at the start of the round."));
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_flashlights", false, Smod2.Config.SettingType.BOOL, true, "Should SPC-343 turn everything into flashlights?"));
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_opendoortime", 300, Smod2.Config.SettingType.NUMERIC, true, "How long in seconds till SPC-343 can open any door."));
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_HP", -1, Smod2.Config.SettingType.NUMERIC, true, "How much health should SCP-343 have. Set to -1 for GodMode."));
-            //this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_nuke_interact", true, Smod2.Config.SettingType.BOOL, true, "Should SPC-343 beable to interact with the nuke."));
+            this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_nuke_interact", true, Smod2.Config.SettingType.BOOL, true, "Should SPC-343 beable to interact with the nuke."));
         }
     }
 }
 
 namespace SCP_343Logic
 {
-	public class MainLogic : IEventHandlerPlayerPickupItem,IEventHandlerRoundStart,IEventHandlerDoorAccess, IEventHandlerSetRole, IEventHandlerPlayerHurt
+	public class MainLogic : IEventHandlerPlayerPickupItem,IEventHandlerRoundStart,IEventHandlerDoorAccess, IEventHandlerSetRole, IEventHandlerPlayerHurt, IEventHandlerWarheadStartCountdown, IEventHandlerWarheadStopCountdown
 
     {
 		Smod2.API.Player TheChosenOne;
@@ -134,5 +138,88 @@ namespace SCP_343Logic
                 ev.Damage = 0;
             }
         }
+
+        public void OnStartCountdown(WarheadStartEvent ev)
+        {
+            if (ev.Activator != null)
+            {
+                if (ev.Activator.TeamRole.Role == Smod2.API.Role.TUTORIAL && ConfigManager.Manager.Config.GetBoolValue("SCP343_nuke_interact", true, false) == false)
+                {
+                    ev.Cancel = false;
+                }
+                else { ev.Cancel = true; }
+            }
+        }
+
+        public void OnStopCountdown(WarheadStopEvent ev)
+        {
+            if(ev.Activator != null)
+            {
+                if (ev.Activator.TeamRole.Role == Smod2.API.Role.TUTORIAL && ConfigManager.Manager.Config.GetBoolValue("SCP343_nuke_interact", true, false) == false)
+                {
+                    ev.Cancel = false;
+                }
+                else { ev.Cancel = true; }
+            }
+        }
     }
 }
+
+namespace SCP_343Commands
+{
+    class SpawnSCP343 : ICommandHandler
+    {
+        private Plugin plugin;
+        public SpawnSCP343(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public string GetCommandDescription()
+        {
+            // This prints when someone types HELP HELLO
+            return "This command spawns in someone as SPC-343. Usage is SpawnSCP343 PlayerID";
+        }
+
+        public string GetUsage()
+        {
+            // This prints when someone types HELP HELLO
+            return "HELLO";
+        }
+
+        public string[] OnCall(ICommandSender sender, string[] args)
+        {
+            // This will print 3 lines in console.
+            //return new string[] { "Hello World!", "My name is example plugin.", "thank you for listening, good bye!" };
+            if (args.Length > 0)
+            {
+                List<Smod2.API.Player> iList = new List<Smod2.API.Player>();
+                List<Int32> iListPlayerId = new List<Int32>();
+                iList = PluginManager.Manager.Server.GetPlayers();
+                foreach (Player Playa in iList)
+                {
+                    if (Playa.PlayerId.ToString() == args[0])
+                    {
+                        plugin.Info(Playa.Name + " Dis the one?");
+                        Player TheChosenOne = Playa;
+                        TheChosenOne.ChangeRole(Smod2.API.Role.TUTORIAL, true, false);
+                        if (ConfigManager.Manager.Config.GetIntValue("SCP343_HP", -1, false) == -1)
+                        {
+                            TheChosenOne.SetGodmode(true);
+                        }
+                        else { TheChosenOne.SetHealth(ConfigManager.Manager.Config.GetIntValue("SCP343_HP", -1, false)); }
+
+                        TheChosenOne.SetRank("red", "SCP-343");
+                        return new string[] { "Made " + Playa.Name + " SCP343!" };
+                    }
+                    else { return new string[] { "Invaild PlayerID" }; }
+
+                }
+                return new string[] { "It worked!" };
+            }
+            else { return new string[] { "You must put a playerid." }; }
+
+        }
+    }
+}
+    
