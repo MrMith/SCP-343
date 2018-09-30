6651b437807c0f5f54be64448b8e56baf9f1f215
Mith <25213249+MrMith@users.noreply.github.com>
2018-09-30 08:33:22 -0500

    Added 3 new config options (SCP343_itemdroplist,SCP343_itemconversionlist, SCP343_itemconvertedlist) and renamed SCP343_itemconvert to SCP343_itemconverttoggle and hopefully fixed the spawn command (If it didn't i left some debug messages in there intentionally) I'm taking a day or 2 break because my brain is mush.

--- SCP-343/SCP-343.cs
+++ SCP-343/SCP-343.cs
@@ -8,6 +8,8 @@ using Smod2.API;
 using Smod2.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace SCP_343
 {
@@ -16,7 +18,7 @@ namespace SCP_343
 	name = "SCP-343",
 	description = "SCP-343 is a passive immortal D-Class Personnel. He spawns with one Flashlight and any item he picks up is instantly morphed into a Flashlight. He seeks to help out who he deems worthy.",
 	id = "Mith.SCP-343",
-	version = "1.08",
+	version = "1.09",
 	SmodMajor = 3,
 	SmodMinor = 1,
 	SmodRevision = 18
@@ -27,7 +29,7 @@ namespace SCP_343
 		{
 			this.Info("SCP-343 has been Disabled.");
 		}
-
+        
 		public override void OnEnable()
 		{
 
@@ -42,14 +44,21 @@ namespace SCP_343
 			this.AddEventHandler(typeof(IEventHandlerSetRole), new MainLogic(this), Priority.Normal);
 			this.AddEventHandler(typeof(IEventHandlerWarheadStartCountdown), new MainLogic(this), Priority.Normal);
 			this.AddEventHandler(typeof(IEventHandlerWarheadStopCountdown), new MainLogic(this), Priority.Normal);
+
             this.AddCommand("SpawnSCP343", new SCP_343Commands.SpawnSCP343(this));
             this.AddCommand("SCP343_Version", new SCP_343Commands.SCP343_Version(this));
 
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_spawnchance", 10f, Smod2.Config.SettingType.FLOAT, true, "Percent chance for SPC-343 to spawn at the start of the round."));
-            this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_flashlights", false, Smod2.Config.SettingType.BOOL, true, "Should SPC-343 turn everything into flashlights?"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_itemconverttoggle", false, Smod2.Config.SettingType.BOOL, true, "Should SPC-343 convert items?"));
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_opendoortime", 300, Smod2.Config.SettingType.NUMERIC, true, "How long in seconds till SPC-343 can open any door."));
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_HP", -1, Smod2.Config.SettingType.NUMERIC, true, "How much health should SCP-343 have. Set to -1 for GodMode."));
             this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_nuke_interact", true, Smod2.Config.SettingType.BOOL, true, "Should SPC-343 beable to interact with the nuke."));
+
+            //https://github.com/Grover-c13/Smod2/wiki/Enum-Lists#itemtype
+            this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_itemdroplist", new int[] {0,1,2,3,4,5,6,7,8,9,10,11,14,17,19,22,27,28,29 }, Smod2.Config.SettingType.NUMERIC_LIST, true, "What items SCP-343 drops instead of picking up."));
+            this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_itemconversionlist", new int[]{13,16,20,21,23,24,25,26}, Smod2.Config.SettingType.NUMERIC_LIST, true, "What items SCP-343 converts."));
+            this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_itemconvertedlist", new int[]{15}, Smod2.Config.SettingType.NUMERIC_LIST, true, "What a item should be converted to."));
+            this.AddConfig(new Smod2.Config.ConfigSetting("SCP343_alloweditems", new int[]{12,15}, Smod2.Config.SettingType.NUMERIC_LIST, true, "What items SPC-343 are allowed to handle."));
         }
     }
 }
@@ -110,13 +119,31 @@ namespace SCP_343Logic
 
 		public void OnPlayerPickupItem(PlayerPickupItemEvent ev)
 		{
-			if (ev.Player.TeamRole.Role == Role.TUTORIAL && ConfigManager.Manager.Config.GetBoolValue("SCP343_flashlights",false,false) == true)
+            if (ev.Player.TeamRole.Role == Role.TUTORIAL && ConfigManager.Manager.Config.GetBoolValue("SCP343_itemconverttoggle", false,false) == true)
 			{
-			    ev.ChangeTo = Smod2.API.ItemType.FLASHLIGHT;
-			}else if (ev.Player.TeamRole.Role == Role.TUTORIAL && ConfigManager.Manager.Config.GetBoolValue("SCP343_flashlights", false, false) == false && PluginManager.Manager.Server.Round.Duration >= 3)
+                int[] itemAllowedList = ConfigManager.Manager.Config.GetIntListValue("SCP343_itemconversionlist", new int[] { 13, 16, 20, 21, 23, 24, 25, 26 }, false);
+
+                if (itemAllowedList.Contains((int)ev.Item.ItemType))
+                {
+                    int[] itemConvertList = ConfigManager.Manager.Config.GetIntListValue("SCP343_itemconvertedlist", new int[] { 15 }, false);
+                    ev.ChangeTo = (ItemType)itemConvertList[RNG.Next(itemConvertList.Length)];
+                }
+                int[] itemBlackList = ConfigManager.Manager.Config.GetIntListValue("SCP343_itemdroplist", new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 17, 19, 22, 27, 28, 29 }, false);
+                if (itemBlackList.Contains((int)ev.Item.ItemType))
+                {
+                    ev.Item.Drop();//Idk how to not have it picked up
+                    ev.Allow = false;// This deletes the item :(
+                }
+            }
+            else if (ev.Player.TeamRole.Role == Role.TUTORIAL && ConfigManager.Manager.Config.GetBoolValue("SCP343_itemconverttoggle", false, false) == false && PluginManager.Manager.Server.Round.Duration >= 3)
             {
-                ev.Item.Drop();//Idk how to not have it picked up
-                ev.Allow = false;
+                int[] itemBlackList = ConfigManager.Manager.Config.GetIntListValue("SCP343_alloweditems", new int[] { 12, 15 }, false);
+                if (!(itemBlackList.Contains((int)ev.Item.ItemType)))
+                {
+                    ev.Item.Drop();//Idk how to not have it picked up
+                    ev.Allow = false;// This deletes the item :(
+                }
+
             } // duration here so 343 can have his first flashlight.
         }
         
@@ -147,7 +174,7 @@ namespace SCP_343Logic
                 else { ev.Cancel = true; }
             }
         }
-
+        
         public void OnStopCountdown(WarheadStopEvent ev)
         {
             if(ev.Activator != null)
@@ -191,21 +218,26 @@ namespace SCP_343Commands
                 iList = PluginManager.Manager.Server.GetPlayers();
                 foreach (Player Playa in iList)
                 {
-                    if (Playa.PlayerId.ToString() == args[0])
+                    plugin.Info(Playa.PlayerId.ToString() + " playerid test");
+                    plugin.Info(args[0] + " arg test");
+                    if (int.TryParse(args[0], out int playaID))
                     {
-                        Player TheChosenOne = Playa;
-                        TheChosenOne.ChangeRole(Smod2.API.Role.TUTORIAL, true, false);
-                        if (ConfigManager.Manager.Config.GetIntValue("SCP343_HP", -1, false) == -1)
+                        if (Playa.PlayerId == playaID)
                         {
-                            TheChosenOne.SetGodmode(true);
+                            Player TheChosenOne = Playa;
+                            TheChosenOne.ChangeRole(Smod2.API.Role.TUTORIAL, true, false);
+                            if (ConfigManager.Manager.Config.GetIntValue("SCP343_HP", -1, false) == -1)
+                            {
+                                TheChosenOne.SetGodmode(true);
+                            }
+                            else { TheChosenOne.SetHealth(ConfigManager.Manager.Config.GetIntValue("SCP343_HP", -1, false)); }
+
+                            TheChosenOne.SetRank("red", "SCP-343");
+                            return new string[] { "Made " + Playa.Name + " SCP343!" };
                         }
-                        else { TheChosenOne.SetHealth(ConfigManager.Manager.Config.GetIntValue("SCP343_HP", -1, false)); }
-
-                        TheChosenOne.SetRank("red", "SCP-343");
-                        return new string[] { "Made " + Playa.Name + " SCP343!" };
+                        else { return new string[] { "Couldn't find player.", "another test " + args.Length }; }
                     }
-                    else { return new string[] { "Invalid PlayerID" }; }
-
+                    else { return new string[] { "Couldn't parse it dawg" }; }
                 }
                 return new string[] { "" };
             }
@@ -228,7 +260,7 @@ namespace SCP_343Commands
 
         public string GetUsage()
         {
-            return "SpawnSCP343 PlayerID";
+            return "SCP343_Version";
         }
 
         public string[] OnCall(ICommandSender sender, string[] args)
